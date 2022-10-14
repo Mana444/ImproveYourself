17bab03175ae5a11e358e35e19a0b5525fe7b2f4
Mana444 <manarii.bremond2022@campus-eni.fr>
2022-10-14 03:06:15 +0200

    Ajout dune activite a Faire implementer Recyclerview

--- app/src/main/java/com/example/improveyourself/MainActivity.java
+++ app/src/main/java/com/example/improveyourself/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.improveyourself;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        Button btnLogin = findViewById(R.id.se_connecter_btn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),RecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        super.onResume();
     }
 }
